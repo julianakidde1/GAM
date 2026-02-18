97e70479e0588859042c16bd5680073862c4e201
Juliana Kidde <kidde1@stolaf.edu>
2026-02-18 10:04:38 -0600

    Initial Source Code Commit

---
+++ GAM_V1/Source/GAM_V1/GAM_V1.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAM_V1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAM_V1, "GAM_V1" );
+
+DEFINE_LOG_CATEGORY(LogGAM_V1)
---
+++ GAM_V1/Source/GAM_V1/GAM_V1GameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAM_V1GameMode.h"
+
+AGAM_V1GameMode::AGAM_V1GameMode()
+{
+	// stub
+}
