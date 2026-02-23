725c01751d4a8ddd98d67a4808aa2886c9d11933
Juliana Kidde <kidde1@stolaf.edu>
2026-02-23 15:20:13 -0600

    Adding base files for ShooterAI Class

---
+++ GAM_V1/Source/GAM_V1/ShooterAI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ShooterAI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM_V1_API AShooterAI : public AAIController
+{
+	GENERATED_BODY()
+	
+};
