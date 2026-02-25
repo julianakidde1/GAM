9c0b165bc5f2fc59e2981ec49a172c3094dd9161
Juliana Kidde <kidde1@stolaf.edu>
2026-02-25 09:39:12 -0600

    Creating PlayerLocation service task.

--- GAM_V1/Source/GAM_V1/BTService_PlayerLocationIfSeen.cpp
+++ GAM_V1/Source/GAM_V1/BTService_PlayerLocationIfSeen.cpp
@@ -3,3 +3,12 @@
 
 #include "BTService_PlayerLocationIfSeen.h"
 
+UBTService_PlayerLocationIfSeen::UBTService_PlayerLocationIfSeen()
+{
+    NodeName = TEXT("Update PlayerLocation If Seen");
+}
+
+void UBTService_PlayerLocationIfSeen::TickNode(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory, float DeltaSeconds)
+{
+    Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
