ded42542fe100ab64d8ca47d3a4f1a357248991b
Juliana Kidde <kidde1@stolaf.edu>
2026-02-25 14:13:58 -0600

    Source code for BT shooting task.

---
+++ GAM_V1/Source/GAM_V1/BTTaskNode_Shoot.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTaskNode_Shoot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM_V1_API UBTTaskNode_Shoot : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
