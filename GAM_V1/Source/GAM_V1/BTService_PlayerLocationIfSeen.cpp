2203ba7d5a6ebff8e801dc00c18ebfe2eae19f09
Juliana Kidde <kidde1@stolaf.edu>
2026-02-25 10:10:59 -0600

    Setting player location if enemy has line of sight.

--- GAM_V1/Source/GAM_V1/BTService_PlayerLocationIfSeen.cpp
+++ GAM_V1/Source/GAM_V1/BTService_PlayerLocationIfSeen.cpp
@@ -3,6 +3,9 @@
 
 #include "BTService_PlayerLocationIfSeen.h"
 
+#include "ShooterAI.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
 UBTService_PlayerLocationIfSeen::UBTService_PlayerLocationIfSeen()
 {
     NodeName = TEXT("Update PlayerLocation If Seen");
@@ -11,4 +14,25 @@ UBTService_PlayerLocationIfSeen::UBTService_PlayerLocationIfSeen()
 void UBTService_PlayerLocationIfSeen::TickNode(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory, float DeltaSeconds)
 {
     Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+    AShooterAI* OwnerController = Cast<AShooterAI>(OwnerComp.GetAIOwner());
+    AGAM_V1Character* Player = OwnerController->PlayerCharacter;
+    UBlackboardComponent* Blackboard = OwnerController->GetBlackboardComponent();
+
+    if (OwnerController && Player && Blackboard) // setting the player location if enemy has line of sight and unsetting it if the enemy doesn't.
+    {
+        if (OwnerController->LineOfSightTo(Player)) 
+       {
+            Blackboard->SetValueAsVector(GetSelectedBlackboardKey(), Player->GetActorLocation());
+            OwnerController->SetFocus(Player);
+       }
+
+       else
+       {
+            Blackboard->ClearValue(GetSelectedBlackboardKey());
+            OwnerController->ClearFocus(EAIFocusPriority::Gameplay);
+       }
+        
+    }
+
 }
