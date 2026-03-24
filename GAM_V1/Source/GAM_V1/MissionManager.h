effffbff7f3af9a6eb91b9460485fc9355f66d4c
Juliana Kidde <kidde1@stolaf.edu>
2026-03-24 11:33:54 -0500

    Initial source code commit.

---
+++ GAM_V1/Source/GAM_V1/MissionManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MissionManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM_V1_API UMissionManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
