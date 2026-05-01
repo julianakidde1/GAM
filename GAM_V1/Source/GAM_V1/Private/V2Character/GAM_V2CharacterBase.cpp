25e372ad4c63ea35d1c1aa220dab82615c3335b8
Juliana Kidde <kidde1@stolaf.edu>
2026-05-01 16:42:59 -0500

    Adding weapon attachment to GAM_V2BaseCharacter.

--- GAM_V1/Source/GAM_V1/Private/V2Character/GAM_V2CharacterBase.cpp
+++ GAM_V1/Source/GAM_V1/Private/V2Character/GAM_V2CharacterBase.cpp
@@ -6,6 +6,10 @@
 AGAM_V2CharacterBase::AGAM_V2CharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
+	
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
 }
 
