2811860c26fe78d544a595974f67b8cf0275bafd
Juliana Kidde <kidde1@stolaf.edu>
2026-05-01 16:29:12 -0500

    Source code for JenniferCharacter and EnemyCharacter classes.

---
+++ GAM_V1/Source/GAM_V1/Public/V2Character/JenniferCharacter.h
@@ -0,0 +1,17 @@
+// Copyright Juliana Kidde
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "V2Character/GAM_V2CharacterBase.h"
+#include "JenniferCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM_V1_API AJenniferCharacter : public AGAM_V2CharacterBase
+{
+	GENERATED_BODY()
+	
+};
