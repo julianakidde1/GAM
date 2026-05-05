435f27b57cfc0b8f2452d58cab93c87c675e6232
Juliana Kidde <julianamwesigwa@gmail.com>
2026-05-05 13:54:50 -0700

    reordering files

--- GAM_V1/Plugins/Procedur07f78a9973f8V10/Source/Pedestrian_System/Private/NPCActionInterface.cpp
+++
@@ -1,5 +0,0 @@
-//  Copyright (c) 2025 KomodoBit Games. All rights reserved.
-
-#include "NPCActionInterface.h"
-
-// Add default functionality here for any INPCActionInterface functions that are not pure virtual.
--- GAM_V1/Plugins/Procedur07f78a9973f8V10/Source/Pedestrian_System/V3/I_NPCSpawningInterface.cpp
+++
@@ -1,5 +0,0 @@
-//  Copyright (c) 2025 KomodoBit Games. All rights reserved.
-
-#include "I_NPCSpawningInterface.h"
-
-// Add default functionality here for any II_NPCSpawningInterface functions that are not pure virtual.
