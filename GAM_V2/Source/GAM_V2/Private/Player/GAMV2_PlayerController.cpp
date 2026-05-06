b82ed0dd38b049f54cec73669e247df09a722a38
Juliana Kidde <julianamwesigwa@gmail.com>
2026-05-06 16:55:37 -0700

    Creating mouse cursor settings.

--- GAM_V2/Source/GAM_V2/Private/Player/GAMV2_PlayerController.cpp
+++ GAM_V2/Source/GAM_V2/Private/Player/GAMV2_PlayerController.cpp
@@ -16,9 +16,15 @@ void AGAMV2_PlayerController::BeginPlay()
 	check(GAM_V2Context); // will halt execution if GAM_V2Context is a nullpointer
 	
 	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
-	check(Subsystem);
-	
-	
+	check(Subsystem)
 	Subsystem->AddMappingContext(GAM_V2Context, 0); 
+	
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default; //choosing the type of mouse cursor
+	
+	FInputModeGameAndUI InputModeData; 
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false); //the cursor remains visible when captured in the viewport
+	SetInputMode(InputModeData);
 	
 }
